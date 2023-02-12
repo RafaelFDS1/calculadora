e7c3b2b8137d77636bed2b4b842e5e2edd7672ce
RafaelFDS1 <rafael_fds@outlook.com>
2023-02-12 20:54:39 -0300

    functions

--- script.js
+++ script.js
@@ -2,34 +2,64 @@ const numbers = Array.from(document.querySelectorAll(".numbers"));
 const output = document.querySelector("#outputs");
 const submit = document.querySelector("#submit");
 const divide = document.querySelector(".divide");
-let firstNumbers = "";
-let activeNumbers = [];
-let contador = 0;
+const clear = document.querySelector(".clear");
+let actualNumber = "";
+let cont = 0;
+
 numbers.forEach((e) => {
   e.addEventListener("click", (e) => {
-    activeNumbers.push(e.target.innerText);
-    if (activeNumbers.length > 0) {
-      output.innerText = activeNumbers.join("");
-    } else {
-      firstNumbers = e.target.innerText;
-      output.innerText += firstNumbers;
-    }
+    actualNumber += e.target.innerText;
+    output.innerText = actualNumber;
+    console.log(cont);
   });
 });
 
 divide.addEventListener("click", (e) => {
   output.innerText += "/";
-  activeNumbers = [];
+  actualNumber += "/";
+  cont++;
+  console.log(cont);
 });
 
-submit.addEventListener("click", (e) => {
+// multply.addEventListener("click", (e) => {
+//   output.innerText += "/";
+//   actualNumber += "/";
+//   cont++;
+// });
+
+clear.addEventListener("click", () => {
+  output.innerText = 0;
+  actualNumber = "";
+  cont = 0;
+});
+
+submit.addEventListener("click", () => {
   output.innerText.split("").forEach((e) => {
-    switch (e) {
-      case "/":
-        output.innerText =
-          Number(output.innerText.split("/")[0]) /
-          Number(output.innerText.split("/")[1]);
-        break;
+    if (cont == 1) {
+      switch (e) {
+        case "/":
+          output.innerText =
+            Number(output.innerText.split("/")[0]) /
+            Number(output.innerText.split("/")[1]);
+          actualNumber = output.innerText;
+          break;
+        case "-":
+          output.innerText =
+            Number(output.innerText.split("-")[0]) -
+            Number(output.innerText.split("-")[1]);
+          break;
+        case "*":
+          output.innerText =
+            Number(output.innerText.split("*")[0]) *
+            Number(output.innerText.split("*")[1]);
+          break;
+        case "+":
+          output.innerText =
+            Number(output.innerText.split("+")[0]) +
+            Number(output.innerText.split("+")[1]);
+          break;
+      }
     }
   });
+  cont = 0;
 });
